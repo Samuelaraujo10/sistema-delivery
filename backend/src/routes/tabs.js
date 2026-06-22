0f72266a495224b556096440799bd31316d35f8e
Samuel Araújo <samuelaraujo71@gmail.com>
2026-06-21 21:08:53 -0300

    fix: Correcao do import de authMiddleware em tabs.js

--- backend/src/routes/tabs.js
+++ backend/src/routes/tabs.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const TabController = require('../controllers/TabController');
-const { auth } = require('../middlewares/auth');
+const { authMiddleware } = require('../middlewares/auth');
 
-router.post('/', auth, TabController.openTab);
-router.get('/establishment/:establishmentId', auth, TabController.getOpenTabs);
-router.post('/:id/close', auth, TabController.closeTab);
+router.post('/', authMiddleware, TabController.openTab);
+router.get('/establishment/:establishmentId', authMiddleware, TabController.getOpenTabs);
+router.post('/:id/close', authMiddleware, TabController.closeTab);
 
 module.exports = router;
